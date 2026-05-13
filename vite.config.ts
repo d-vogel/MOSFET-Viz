0e9f529267468cd23340cb3c1320bc0831d72372
Dorian Vogel <dorian.vogel@fhnw.ch>
2026-05-13 15:22:41 +0200

    Fix GitHub Pages base path for production build

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import preprocess from 'svelte-preprocess'
 
-export default defineConfig({
-  base: '/',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/MOSFET-Viz/' : '/',
   plugins: [
     svelte({
       preprocess: preprocess()
     })
   ],
-})
+}))
