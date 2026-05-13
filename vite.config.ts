a8d15d70f58875c98e0e8c5e5806eb893a156aa7
Dorian Vogel <dorian.vogel@fhnw.ch>
2026-05-13 14:12:26 +0200

    Added github pages deployement

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import preprocess from 'svelte-preprocess'
 
 export default defineConfig({
+  base: '/mosfet-animation/',
   plugins: [
     svelte({
       preprocess: preprocess()
